3dea270b8ffce3eae691074a3e4110c3d9fc52ce
Cindy-Onwonga <cindykerubo@gmail.com>
2023-03-23 16:34:53 -0700

    monty

--- monty.h
+++ monty.h
@@ -8,28 +8,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <fcntl.h>
-
-/**monty errors defined*/
-#define MONTY_ERROR_NONE 0
-#define MONTY_ERROR_INVALID_OPCODE 1
-#define MONTY_ERROR_PUSH_MISSING_ARG 2
-#define MONTY_ERROR_PUSH_INVALID_ARG 3
-#define MONTY_ERROR_PINT_EMPTY 4
-#define MONTY_ERROR_POP_EMPTY 5
-
-
-typedef struct monty_s{
-  char  *save_ptr;
-  int line;
-  char *token;
-  int mode;
-  int error;
-}monty_t;
-
-extern char* operand;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -99,20 +77,4 @@ void _pstr(stack_t **stack, unsigned int line_number);
 
 void _free(stack_t *stack);
 int _isdigit(char *str);
-
-line_t *textfile_to_array(const char *filename);
-void op_push(stack_t **stack, unsigned int line_number);
-void op_pall(stack_t **stack, unsigned int line_number);
-void op_pint(stack_t **stack, unsigned int line_number);
-void op_pop(stack_t **stack, unsigned int line_number);
-void op_swap(stack_t **stack, unsigned int line_number);
-
-char **split_line(char *line);
-void (*get_op_func(char *s))(stack_t**, unsigned int);
-
-void free_lines(line_t *head);
-void free_stack(stack_t *head);
-int _atoi(char *s, int* n);
-
-
 #endif /* MONTY_H */
